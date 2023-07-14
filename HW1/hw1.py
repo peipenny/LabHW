6ce25cdaeaf1a5daadbfc2022f83e887bd686cba
peipenny <penny5462254@gmail.com>
2023-07-14 11:55:32 +0800

    0714

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,14 +1,16 @@
 import itertools
+import time
 
 def evaluate_solution(solution):
     return sum(solution)
 
-def exhaustive_search(problem_size):
+def exhaustive_search(problem_size, max_runtime):
     best_solution = None
     best_fitness = 0
 
     all_solutions = itertools.product([0, 1], repeat=problem_size)
 
+    start_time = time.time()
     i = 0
     for solution in all_solutions:
         fitness = evaluate_solution(solution)
@@ -19,10 +21,17 @@ def exhaustive_search(problem_size):
         print(f"iteration {i+1} - sol: {solution}, fitness: {fitness}")
         i += 1
 
+        current_time = time.time()
+        runtime = current_time - start_time
+        if runtime >= max_runtime:
+            break
+
     return best_solution, best_fitness
 
 problem_size = 100
-best_solution, best_fitness = exhaustive_search(problem_size)
+max_runtime = 30 * 60  # 30 minutes
+
+best_solution, best_fitness = exhaustive_search(problem_size, max_runtime)
 
 print("sol:", best_solution)
 print("fitness:", best_fitness)
